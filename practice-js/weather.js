003088b5018472fc74b9d8d23f8f00c9bb0bf38a
Your Name <258510@st.takushoku-u.ac.jp>
2026-06-18 15:33:58 +0900

    あらあら

--- practice-js/weather.js
+++
@@ -1,9 +0,0 @@
-function print(data) {
-    console.log("都市名: " + data.name);
-    console.log("天気: " + data.weather[0].description);
-    console.log("最低気温: " + data.main.temp_min);
-    console.log("最高気温: " + data.main.temp_max);
-    console.log("湿度: " + data.main.humidity);
-    console.log("風速: " + data.wind.speed);
-    console.log("風向: " + data.wind.deg);
-}
