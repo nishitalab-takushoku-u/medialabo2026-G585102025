3b1c5f58b5f6a2fa3025575d1717b181348bfac1
Your Name <258510@st.takushoku-u.ac.jp>
2026-06-18 15:33:42 +0900

    あああ

--- weather.js
+++ weather.js
@@ -1,7 +1,14 @@
 
 // 課題3-2 のプログラムはこの関数の中に記述すること
-function print(data) {
-
+function print(data) {function print(data) {
+    console.log("都市名: " + data.name);
+    console.log("天気: " + data.weather[0].description);
+    console.log("最低気温: " + data.main.temp_min);
+    console.log("最高気温: " + data.main.temp_max);
+    console.log("湿度: " + data.main.humidity);
+    console.log("風速: " + data.wind.speed);
+    console.log("風向: " + data.wind.deg);
+}
 }
 
 // 課題5-1 の関数 printDom() はここに記述すること
